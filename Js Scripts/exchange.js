c75c249da7f9d4626359c45e64486fbaed5034e7
Kenny Marcano <Kenny.Marcano@fdmgroup.com>
2020-09-16 15:51:24 +0100

    updated as of 4

---
+++ Js Scripts/exchange.js
@@ -0,0 +1,30 @@
+// IIFE Immediatley invoked function expression
+(function(){
+    //code goes here
+    // console.info("I was called")
+    // VAR is 
+    var pricePounds;
+    var exchangeRate = 0.87;
+    var priceEuros =0;
+
+    var convertForm = document.getElementById("myForm");
+    var msg = document.getElementById("msg");
+
+    convertForm.addEventListener("submit", function(ev){
+        console.dir(ev);
+        ev.preventDefault();
+        pricePounds = parseFloat (document.getElementById("pounds").value);
+        console.info(pricePounds);
+        if(isNaN(pricePounds)){
+            msg.innerHTML = "Must be a number"
+            msg.setAttribute("class","error")
+        }else{
+            msg.removeAttribute("class")
+        priceEuros = pricePounds *exchangeRate;
+        msg.innerHTML = `&euro; ${priceEuros}`
+    }
+
+        // console.info("form submitted")
+    })
+
+})();
